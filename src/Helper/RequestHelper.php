2d21b4f8c348bc5dea018c89ac3ceffa8274718c
felipeoliveiranet <felipeoliveiranet@gmail.com>
2020-08-24 20:22:30 -0300

    update: fix cors

--- src/Helper/RequestHelper.php
+++ src/Helper/RequestHelper.php
@@ -27,6 +27,14 @@ class RequestHelper
                 //->withHeader('Access-Control-Allow-Origin', '"*"');
                 //->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
                 //->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+            /*
+            $http_origin = $_SERVER['HTTP_ORIGIN'];
+
+            if ($http_origin == "http://www.domain1.com" || $http_origin == "http://www.domain2.com" || $http_origin == "http://www.domain3.com")
+            {
+                header("Access-Control-Allow-Origin: $http_origin");
+            }
+            */
         });
     }
 
