168084e0f294f7b54fe805fc8c871388a2af133d
felipeoliveiranet <felipeoliveiranet@gmail.com>
2020-08-24 20:39:05 -0300

    update: fix cors

--- routes.php
+++ routes.php
@@ -10,23 +10,30 @@ use Slim\App;
 
 return function(App $app) {
 
+    /*
     $app->redirect('', '/', 200);
     $app->get('/', function(Request $request, Response $response, $args) {
 
         $request->getBody()->write("Welcome to the great Todolist of AWS Elastic Beanstalk and DynamoDB \o/");
     });
+    */
 
-    $app->group('/tasks', function ($app) {
+    $app->group('/tasks', function (App $app) {
 
         $app->get('/', [TasksController::class, 'list']);
         $app->post('/', [TasksController::class, 'insert']);
         $app->delete('/', [TasksController::class, 'delete']);
+        $app->options("/", function(Request $request, Response $response, $args) { return $response->withStatus(200); });
+        $app->options("", function(Request $request, Response $response, $args) { return $response->withStatus(200); });
 
         $app->group('/{id_task}', function ($app) {
 
             $app->get('', [TasksController::class, 'get']);
             $app->patch('', [TasksController::class, 'update']);
             $app->patch('/{task_status}', [TasksController::class, 'updateStatus']);
+            $app->delete('', [TasksController::class, 'delete']);
+            $app->options("", function(Request $request, Response $response, $args) { return $response->withStatus(200); });
+            $app->options("/", function(Request $request, Response $response, $args) { return $response->withStatus(200); });
         });
     });
 };
--- src/Helper/RequestHelper.php
+++ src/Helper/RequestHelper.php
@@ -22,19 +22,7 @@ class RequestHelper
 
             $response = $res->handle($req, $res);
 
-            return $response;
-                //->withHeader('Content-type', 'application/json');
-                //->withHeader('Access-Control-Allow-Origin', '"*"');
-                //->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
-                //->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-            /*
-            $http_origin = $_SERVER['HTTP_ORIGIN'];
-
-            if ($http_origin == "http://www.domain1.com" || $http_origin == "http://www.domain2.com" || $http_origin == "http://www.domain3.com")
-            {
-                header("Access-Control-Allow-Origin: $http_origin");
-            }
-            */
+            return $response->withHeader('Content-type', 'application/json');
         });
     }
 
