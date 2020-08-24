894291b9ff03cc02d6dbe27720d00cace6d07d1d
felipeoliveiranet <felipeoliveiranet@gmail.com>
2020-08-24 19:20:29 -0300

    update: fix cors

--- src/Helper/RequestHelper.php
+++ src/Helper/RequestHelper.php
@@ -23,7 +23,7 @@ class RequestHelper
             $response = $res->handle($req, $res);
 
             return $response
-                ->withHeader('Content-type', 'application/json')
+                ->withHeader('Content-type', 'application/json');
                 //->withHeader('Access-Control-Allow-Origin', '"*"');
                 //->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
                 //->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
