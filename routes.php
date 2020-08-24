048f7cd63963a2525bb531e0a44cc75377dfdb1a
felipeoliveiranet <felipeoliveiranet@gmail.com>
2020-08-23 22:21:58 -0300

    update: routes

--- routes.php
+++ routes.php
@@ -4,9 +4,16 @@
 
 use App\Controller\TasksController;
 
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\App;
 
 return function(App $app) {
+
+    $app->get('/', function(Request $request, Response $response, $args) {
+
+        $request->getBody()->write("Welcome to the great Todolist of AWS Elastic Beanstalk \o/");
+    });
 
     $app->group('/tasks', function ($app) {
 
